37657a377181c0c4b8906f7c51894d032d97e578
aleksmn <al-mn@yandex.ru>
2023-12-07 21:15:39 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+blog = {
+    1: {
+        "title": "Пишем API на Python",
+        "author": "artur123",
+        "keywords": ["API", "блог",  "FastAPI"],
+    },
+    2: {
+        "title": "Используем словари для храниения данных",
+        "author": "roma321",
+        "keywords": ["Python", "словарь", "данные"],
+    },
+    3: {
+        "title": "Операции со множествами",
+        "author": "artur123",
+        "keywords": ["множество", "Python", "данные"],
+    },
+}
+
+
+@app.get("/")
+def home():
+    return {"Data":"test"}
+
+
+@app.get("/about")
+def about():
+    return {"Data": "about the project"}
+
+
+@app.get("/post/{post_id}")
+def get_post(post_id: int):
+    return blog[post_id]
+
+
+@app.get("/keyword/{keyword}")
+def get_post(keyword: str):
+    posts = []
+    for post in blog:
+        if keyword in blog[post]["keywords"]:
+            posts.append(blog[post])
+    return posts
